4036c82cc0723120f9e5ec8659b4511b6853e523
Robin Scavo <sirscavo@gmail.com>
2022-07-13 15:01:22 -0600

    pre-deploy

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
+const path = require('path');
 const express = require('express');
-// const colors = require('colors');
 const dotenv = require('dotenv').config();
 const cors = require("cors");
 const { errorHandler } = require('./middleware/error');
 const connectDB = require('./config/db')
 const users = require('./routes/usersRoute');
 const decks = require('./routes/decksRoute');
+console.log('!!!!!!!!!', process.env.PORT)
 
 const port = process.env.PORT || 8080;
 
@@ -19,6 +20,15 @@ app.use(cors())
 
 app.use('/decks', decks)
 app.use('/users', users);
+
+// Serve front end
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler);
 
